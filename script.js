1cec50e167a3d5348e054fe059c7b5f89cf7ca31
MCamus10 <109302595+MCamus10@users.noreply.github.com>
2025-12-09 22:26:25 -0300

    Changed from flex to table

--- script.js
+++ script.js
@@ -8,71 +8,67 @@ function Book(title, author, pages, read, id) {
     this.id = id;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const id = crypto.randomUUID()
+function addBookToLibrary(title, author, pages, read, id) {
     const book = new Book(title, author, pages, read, id);
     myLibrary.push(book);
+
 }
 
 //Add new book:
 const newBookForm = document.querySelector("#new-book");
 newBookForm.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const bookName = document.querySelector("#book-name").value;
-    const bookAuthor = document.querySelector("#book-author").value;
-    const bookPages = document.querySelector("#book-pages").value;
-    const bookRead  = document.querySelector("input[name='is-read']:checked").value === "true"; //convierte bookRead a boolean.
-    addBookToLibrary(bookName, bookAuthor, bookPages, bookRead);
+    const title = document.querySelector("#book-name").value;
+    const author = document.querySelector("#book-author").value;
+    const pages = document.querySelector("#book-pages").value;
+    const read  = document.querySelector("input[name='is-read']:checked").value === "true"; //convierte bookRead a boolean.
+    const uniqueId = crypto.randomUUID();
+    addBookToLibrary(title, author, pages, read, uniqueId);
     document.getElementById("new-book").reset();
 
     //Books quantity
     const booksQtty = document.querySelector("#total-books");
-    booksQtty.textContent = `Total books stored: ${myLibrary.length}`
-})
+    booksQtty.textContent = `Total books stored: ${myLibrary.length}`;
 
-//Show book list with button
-const showBtn = document.querySelector("#show-btn");
-const booksContainer = document.querySelector("#books-container");
-showBtn.addEventListener("click", () => {
-    booksContainer.innerHTML = "";
-    for (let i = 0; i < myLibrary.length; i++) {
-        const bookCard = document.createElement("div");
-        bookCard.className = "book-card";
-        
-        const bookName = document.createElement("p");
-        bookName.textContent = `Title: ${myLibrary[i].title}`;
-        bookCard.appendChild(bookName);
-
-        const bookAuthor = document.createElement("p");
-        bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
-        bookCard.appendChild(bookAuthor);
-
-        const bookPages = document.createElement("p")
-        bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
-        bookCard.appendChild(bookPages);
-
-        const bookRead = document.createElement("p");
-        bookRead.textContent = myLibrary[i].read ? "This book is already read" : "This book has not been read";
-        bookCard.appendChild(bookRead);
-
-        const bookId = document.createElement("p");
-        bookId.textContent = `ID: ${myLibrary[i].id}`;
-        bookCard.appendChild(bookId);
-
-        const removeButton = document.createElement("button");
-        removeButton.className = "delete-book-btn";
-        removeButton.textContent = "Delete";
-        removeButton.addEventListener("click", ()=>{
-            //code to remove book;
-        })
-        bookCard.appendChild(removeButton);
-
-        document.querySelector("#books-container").appendChild(bookCard);
+    //Ad d book to table
+    const booksTable = document.querySelector("#books-table");
+    // for (let i = 0; i < myLibrary.length; i++) {
+    const bookRow = document.createElement("tr");
+    bookRow.id = uniqueId;
     
-    }
-})
+    const bookName = document.createElement("td");
+    bookName.textContent = title;
+    bookRow.appendChild(bookName);
 
+    const bookAuthor = document.createElement("td");
+    bookAuthor.textContent = author;
+    bookRow.appendChild(bookAuthor);
 
+    const bookPages = document.createElement("td")
+    bookPages.textContent = pages;
+    bookRow.appendChild(bookPages);
+
+    const bookRead = document.createElement("td");
+    bookRead.textContent = read ? "Yes" : "No";
+    bookRow.appendChild(bookRead);
+
+    const bookId = document.createElement("td");
+    bookId.textContent = uniqueId;
+    bookRow.appendChild(bookId);
+    booksTable.appendChild(bookRow);
+
+  /*   const removeButton = document.createElement("button");
+    removeButton.className = "delete-book-btn";
+    removeButton.textContent = "Delete";
+    removeButton.addEventListener("click", ()=>{
+        //code to remove book;
+    })
+    bookCard.appendChild(removeButton); */
+
+    
+
+    // };
+});
 
 
 
